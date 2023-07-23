0e58a6c19ff5127a127f4300a70a4cb16cdfb445
appleman <appleman@something.com>
2023-07-23 14:02:42 +0100

    Fixed syntax error on 'bulbasaur.pp'
    Accidentally wrote '=<' instead of '=>'. Corrected.

--- manifests/bulbasaur.pp
+++ manifests/bulbasaur.pp
@@ -1,4 +1,4 @@
 file { '/home/user/bulbasaur.txt':
     ensure => 'file',
-    content =< 'Number 001\nKanto Region\nGrass Pokemon\n'>
+    content => 'Number 001\nKanto Region\nGrass Pokemon\n'>
 }
