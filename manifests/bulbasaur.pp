ae28fcbfcfaf5d6570b711d866beb87ba058bb90
orangeman <orangeman@example.com>
2023-08-09 12:02:42 +0100

    Changed "<" to ","
    Fixes typo.
    
    Also, made sure the "=>" were all lined up.

--- manifests/bulbasaur.pp
+++ manifests/bulbasaur.pp
@@ -1,4 +1,4 @@
 file { '/home/user/bulbasaur.txt':
-    ensure => 'file',
-    content => 'Number 001\nKanto Region\nGrass Pokemon\n'>
+    ensure  => 'file',
+    content => 'Number 001\nKanto Region\nGrass Pokemon\n',
 }
